5d0930b784f3f4374f1610e34fa1c2b6ebd53756
yogeshrajendran-24 <yogeshrajendran2410@gmail.com>
2025-12-09 19:37:25 +0530

    updated again

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,7 +6,7 @@ describe("GET /", () => {
   it("should return greeting text", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("Hello from CI/CD demo!");
+    expect(res.text).toBe("Hello Yogesh, Hello from CI/CD demo!");
   });
 });
 
